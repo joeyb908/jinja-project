01a74faa405a7e6e7e3683e2dd8100076feeb0ef
Joey Ohannesian <joeyb908@gmail.com>
2022-07-15 22:05:27 +0000

    Added /guess/<name> that takes your name and gives you a projected age and gender.

--- server.py
+++ server.py
@@ -13,9 +13,18 @@ def home():
     random_number = r.randint(1, 10)
     return render_template('index.html', num=random_number, current_year=year, name=name)
 
+
 @app.route('/guess/<name>')
 def guess(name):
-    return render_template()
+    connection = requests.get(f'https://api.agify.io?name={name.lower()}')
+    data = connection.json()
+    age = int(data['age'])
+    connection.close()
+    connection = requests.get(f'https://api.genderize.io/?name={name}')
+    data = connection.json()
+    gender = data['gender']
+    return render_template('guess.html', name=name.capitalize(), age=age, gender=gender)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
